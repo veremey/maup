4956fbfbbfe321e7c76e0a842b880da69ace3889
Сергей <Veremey@i.ua>
2015-02-11 21:03:28 +0200

    strt

---
+++ js/script.js
@@ -0,0 +1,125 @@
+$(function(){
+    initSlider();
+    initDrop();
+    initAccordion();
+    $('.header>.cam-wrap>.search-form input[type="text"]').width($('.cam-wrap').width() - $('.lang-box').innerWidth() - $('.top-menu').width() - $('.log-in').width() - 30)
+});
+
+$(window).resize(function(){
+    $('.header>.cam-wrap>.search-form input[type="text"]').width($('.cam-wrap').width() - $('.lang-box').innerWidth() - $('.top-menu').width() - $('.log-in').width() - 30)
+})
+
+function initSlider() {
+    if($('.slider').length) {
+        $('.slider').bxSlider({
+            speed: 800
+        });
+    }
+}
+
+function initDrop() {
+    if($('.drop-section').length) {
+        var opener = $('.lang-box a, .top-menu a'),
+            parent = $('.drop-section'),
+            closeBtn = parent.find('.close-drop'),
+            drops = $('.drop-section .cam-wrap>div');
+
+        opener.on('click', function(e){
+            var id = $(this).attr('href').split('#').pop(),
+                el = $(this);
+            drops.each(function(){
+                var drop = $(this);
+                if($(this).attr('id') == id) {
+                    drops.hide();
+                    drop.show();
+                    parent.show();
+                    opener.parent().removeClass('active');
+                    el.parent().addClass('active');
+                }
+            });
+
+            e.preventDefault();
+        });
+
+        closeBtn.on('click', function(e){
+            opener.parent().removeClass('active');
+            parent.hide();
+            drops.hide();
+
+            e.preventDefault();
+        });
+
+        $('.search-btn').on('click', function(e) {
+            $('#search-drop').toggle();
+            $('#search-drop').parents('.drop-section').toggle();
+
+            e.preventDefault();
+        });
+        $('.open-menu').on('click', function(e) {
+            $('#menu-mobile').toggle();
+            $('#menu-mobile').parents('.drop-section').toggle();
+
+            e.preventDefault();
+        });
+        $('.btn-lang').on('click', function(e) {
+            $('#lang-mobile').toggle();
+            $('#lang-mobile').parents('.drop-section').toggle();
+
+            e.preventDefault();
+        });
+    }
+    if($('.inner-nav').length && $(window).width() < 600) {
+        $('.has-drop a').on('click', function(e){
+            $('ul.dropdown-menu').hide();
+            if($(this).parents('ul.dropdown-menu').length) {
+                $(this).parents('ul.dropdown-menu').show();
+                $(this).parent().find('ul.dropdown-menu').find('>li.back span').html('&larr; ' + $(this).parent().find('>a').text())
+            }
+            $(this).parent().find('ul.dropdown-menu').show();
+            $(this).parents('ul.inner-page-menu').animate({
+                'left':parseInt($(this).parents('ul.inner-page-menu').css('left'))-$(this).innerWidth()
+            })
+            e.preventDefault();
+        })
+        $('.inner-page-menu>li .dropdown-menu .back').on('click', function(){
+            $(this).parents('ul.inner-page-menu').animate({
+                'left':parseInt($(this).parents('ul.inner-page-menu').css('left'))+$(this).parent().width()
+            })
+        })
+        $('.mobile-heading a.open').on('click', function(e) {
+            $(this).toggleClass('active');
+            $(this).parent().next('.inner-page-menu').toggleClass('active');
+            e.preventDefault();
+        })
+    }
+}
+
+function initAccordion() {
+    if($('.accordion').length) {
+        $('.accordion .opener').on('click', function(e){
+            if(!$(this).hasClass('active')) {
+                $('.accordion .slide').slideUp();
+                $('.accordion .opener').removeClass('active');
+                $(this).next('.slide').slideDown();
+                $(this).addClass('active');
+            } else {
+                $(this).next('.slide').slideUp();
+                $(this).removeClass('active');
+            }
+            e.preventDefault();
+        })
+        $('.accordion .hide').on('click', function(e){
+            $(this).parent().slideUp();
+            $(this).parent().prev('.opener').removeClass('active');
+            e.preventDefault();
+        })
+    }
+}
+
+
+function newFunc(element) {
+
+}
+$(function(){
+    newFunc($('.article-title'));
+})
