879e5f5cc08f65ca31914cfda9d79a9ba8a2547f
Сергей <Veremey@i.ua>
2015-02-12 18:57:01 +0200

    cmp

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 $(function(){
+    function tab() {
+        $(".js-tab").each(function(){
+
+            var tab_link = $(this).find("a");
+            var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+            tab_cont.hide();
+            $(this).parents(".js-tab-group").find(".js-tab1").show();
+
+            tab_link.bind("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).slideDown("fast");
+                return false;
+            });
+        });
+    }
+    tab();
+});
+
+
+$(function(){
     initSlider();
     initDrop();
     initAccordion();
     $('.header>.cam-wrap>.search-form input[type="text"]').width($('.cam-wrap').width() - $('.lang-box').innerWidth() - $('.top-menu').width() - $('.log-in').width() - 30)
 });
+
+
+
+
+
+
+
 
 $(window).resize(function(){
     $('.header>.cam-wrap>.search-form input[type="text"]').width($('.cam-wrap').width() - $('.lang-box').innerWidth() - $('.top-menu').width() - $('.log-in').width() - 30)
@@ -123,3 +155,4 @@ function newFunc(element) {
 $(function(){
     newFunc($('.article-title'));
 })
+
